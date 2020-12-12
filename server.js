704d8799b28d48b14ff852bb2071dc8e90fa0c0c
Danny Maier <dannymaier@gmail.com>
2020-12-12 13:32:08 -0800

    add new test route

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ const PORT = process.env.PORT || 3001;
 app.get('/', function(req,res){
     res.send("Hello World")
 })
+app.get('/test', function(req,res){
+    res.send("Hello Test World")
+})
 app.listen(3001)
