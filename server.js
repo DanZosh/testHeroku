6c65042f91b1b147c4539f3d22a03432594244be
Danny Maier <dannymaier@gmail.com>
2020-12-12 13:37:43 -0800

    fixed error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const test = nonExistingVariable;
+// const test = nonExistingVariable;
 
 
 app.get('/', function(req,res){
